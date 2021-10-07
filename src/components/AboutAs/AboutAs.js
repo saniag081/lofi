9176329a1e77eeafa9246ec9d7bd379fa6d36a01
saniag081 <sgonzalezhernandez081@gmail.com>
2021-10-07 13:05:41 -0500

    feat(AboutAs): se crea componente y se agregan estilos

---
+++ src/components/AboutAs/AboutAs.js
@@ -0,0 +1,18 @@
+import './AboutAs.scss'
+
+function AboutAs() {
+	return (
+		<section className="about">
+			<div className="u-content">
+				<div className="about__container">
+					<h2 className="about__title">MEMBERSHIPS</h2>
+					<h6 className="about__subtitle">WHILE THE BAR WILL BE OPEN TO THE PUBLIC, THERE ARE MEMBERSHIP OPPORTUNITIES AVAILABLE THAT WILL EXPEDITE NIGHTLY SEATINGS AND ALLOW FOR ACCESS TO EXCLUSIVE EVENTS. DANTE’S HIFI WILL HOST AT LEAST ONE MEMBER’S ONLY EVENT A MONTH.</h6>
+					<a className="u-btn--secundary" href="#4">EXPLORE MEMBERSHIPS</a>
+				</div>
+				<p className="about__paragraph">MEMBERSHIP WILL AUTOMATICALLY RENEW EACH YEAR, PROVIDED THE MEMBER CONTINUES TO MEET STANDARDS. PRICES ARE SUBJECT TO CHANGE ANNUALLY. OWNERSHIP RESERVES THE RIGHT TO REVOKE MEMBERSHIP AT ANY TIME AT THEIR DISCRETION.</p>
+			</div>
+		</section>
+	)
+}
+
+export default AboutAs;
