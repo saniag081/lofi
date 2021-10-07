0950d7e5e15512e9e898f6d91bf442bc9a596515
saniag081 <sgonzalezhernandez081@gmail.com>
2021-10-06 22:37:27 -0500

    style(utils): se agrega secundary btn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,18 @@ import './Header.scss'
 function Header() {
 	return (
 		<header className="u-content c-header">
-			<figure className="c-header-figure">
+			<figure className="c-header__figure">
 				<img src="./logo.svg" alt="logo lofi" />
 			</figure>
-			<nav className="c-header-nav">
-				<ul className="c-header-nav-list">
-					<li className="c-header-nav-list-item">
+			<nav className="c-header__nav">
+				<ul className="c-header__list">
+					<li className="c-header__list__item">
 						<a href="#2">RESERVE</a>
 					</li>
-					<li className="c-header-nav-list-item">
+					<li className="c-header__list__item">
 						<a href="#3">MEMBERS</a>
 					</li>
-					<li className="c-header-nav-list-item">
+					<li className="c-header__list__item">
 						<a href="#4">in</a>
 					</li>
 				</ul>
--- src/components/Introducción/Introduccion.js
+++ src/components/Introducción/Introduccion.js
@@ -6,10 +6,10 @@ function Introduccion() {
 		<section className="c-instroduccion">
 			<Header />
 			<div className="u-content">
-				<h1 className="c-instroduccion-title">HIFI SOUND</h1>
-				<h2 className="c-instroduccion-title">LOFI APPROACH</h2>
-				<h4 className="c-instroduccion-subtitle">MIAMI’S FIRST-EVER VINYL LISTENING BAR.</h4>
-				<a className="u-btn" href="#1">RESERVE NOW</a>
+				<h1 className="c-instroduccion__title">HIFI SOUND</h1>
+				<h2 className="c-instroduccion__title">LOFI APPROACH</h2>
+				<h4 className="c-instroduccion__subtitle">MIAMI’S FIRST-EVER VINYL LISTENING BAR.</h4>
+				<a className="u-btn--primary" href="#1">RESERVE NOW</a>
 			</div>
 		</section>
 	)
